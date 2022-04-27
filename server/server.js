ba2661cf6ca580f4b1e6a6f305adcc7f76755263
David <dilic3320rn@raf.rs>
2022-04-27 05:47:31 +0200

    Prepare for deployment

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js"
 import morgan from 'morgan'
+import path from 'path';
 
 dotenv.config()
 
@@ -19,9 +20,6 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Home")
-})
 
 app.use('/api/product', productRoutes)
 app.use('/api/users', userRoutes)
@@ -30,6 +28,19 @@ app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))) 
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
